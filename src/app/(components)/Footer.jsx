7a00ca6734a5108fd32c1ccddd4fb5b02a07bf63
SuchetaVadakkepat10 <124446549+SuchetaVadakkepat10@users.noreply.github.com>
2024-01-12 21:22:53 +0400

    Modified footer

--- src/app/(components)/Footer.jsx
+++ src/app/(components)/Footer.jsx
@@ -16,8 +16,8 @@ const Footer = () => {
 
   return (
     <div className="relative bg-[url('/frame.png')] bg-cover bg-no-repeat bg-[top] text-center">
-      <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 py-8 flex flex-col md:flex-row md:gap-10">
-        <div className="flex flex-col items-center md:items-start gap-6 md:w-2/6">
+      <div className="max-w-screen-xl mx-auto px-2 sm:px-6 lg:px-8 py-8 flex flex-col md:flex-row md:gap-10">
+        <div className="flex flex-col items-center md:items-start gap-2 md:w-4/6">
           <div className="font-semibold text-whitesmoke">
             <Image
               src="/nav_logo.png"
@@ -32,60 +32,68 @@ const Footer = () => {
             </p>
           </div>
         </div>
-        <div className="flex flex-col items-start gap-6 text-whitesmoke md:w-4/6 md:gap-10">
+
+
+        
+        <div className="flex flex-row items-start gap-6 text-whitesmoke md:w-2/6 md:gap-16 mt-4">
           {/* PC Version - Two Columns */}
-          <div className="hidden md:flex md:flex-row md:gap-10">
-            <div className="flex flex-col items-start gap-6 text-whitesmoke">
-              <div className="font-semibold">Innovation Grant and Support</div>
+          <div className="hidden md:flex  md:gap-10 ">
+            <div className="flex flex-col items-start gap-6 text-whitesmoke mt-4">
+              <div className="font-semibold text-left"><h3>Innovation Grant and Support</h3></div>
               <a
-                className="cursor-pointer"
+                className="cursor-pointer text-whitesmoke hover:text-black"
                 href="/pdfs/Advance_Request_Form.pdf"
                 download="Advance_Request_Form.pdf"
+                style={{ textDecoration: 'none' }}
               >
                 Advance Request Form
               </a>
               <a
-                className="cursor-pointer"
+                className="cursor-pointer  text-whitesmoke hover:text-black"
                 href="/pdfs/Budget_Request_Form.pdf"
                 download="Budget_Request_Form.pdf"
+                style={{ textDecoration: 'none' }}
               >
                 Budget Request Form
               </a>
               <a
-                className="cursor-pointer"
+                className="cursor-pointer  text-whitesmoke hover:text-black"
                 href="/pdfs/Reimbursement_Form.pdf"
                 download="Reimbursement_Form.pdf"
+                style={{ textDecoration: 'none' }}
               >
                 Reimbursement Form
               </a>
-            </div>
 
-            <div className="flex flex-col items-start gap-6 text-whitesmoke">
               <a
-                className="cursor-pointer"
+                className="cursor-pointer  text-whitesmoke hover:text-black"
                 href="/pdfs/SA_Tech_Advance_Settlement_Form.pdf"
                 download="SA_Tech_Advance_Settlement_Form.pdf"
+                style={{ textDecoration: 'none' }}
               >
                 Advance Settlement Form
               </a>
               <a
-                className="cursor-pointer"
+                className="cursor-pointer  text-whitesmoke hover:text-black"
                 href="/pdfs/SA_Tech_03_ASSET_TRANSFER_FORM.pdf"
                 download="SA_Tech_03_ASSEST_TRANSFER_FORM.pdf"
+                style={{ textDecoration: 'none' }}
               >
                 Asset Transform Form
               </a>
               <a
-                className="cursor-pointer"
+                className="cursor-pointer  text-whitesmoke hover:text-black"
                 href="/pdfs/SA_Tech_Non_Consumable_Indent.pdf"
                 download="SA_Tech_Non_Consumable_Indent.pdf"
+                style={{ textDecoration: 'none' }}
               >
                 Non Consumable Indent Form
               </a>
               <a
-                className="cursor-pointer"
+                className="cursor-pointer  text-whitesmoke hover:text-black"
                 href="/pdfs/SA_Tech_Temporary_Advance_Form.pdf"
                 download="SA_Tech_Temporary_Advance_Form.pdf"
+                style={{ textDecoration: 'none' }}
               >
                 Temporary Advance Form
               </a>
@@ -94,75 +102,89 @@ const Footer = () => {
 
           {/* Mobile Version - Single Column */}
           <div className="md:hidden">
-            <div className="flex flex-col items-start gap-6 text-whitesmoke">
-              <div className="font-semibold">Innovation Grant and Support</div>
+            <div className="flex flex-col items-start gap-6 text-whitesmoke mt-4">
+              <div className="font-semibold text-left"><h3>Innovation Grant and Support</h3></div>
               <a
-                className="cursor-pointer"
+                className="cursor-pointer text-whitesmoke hover:text-black"
                 href="/pdfs/Advance_Request_Form.pdf"
                 download="Advance_Request_Form.pdf"
+                style={{ textDecoration: 'none' }}
               >
                 Advance Request Form
               </a>
               <a
-                className="cursor-pointer"
+                className="cursor-pointer text-whitesmoke hover:text-black"
                 href="/pdfs/Budget_Request_Form.pdf"
                 download="Budget_Request_Form.pdf"
+                style={{ textDecoration: 'none' }}
               >
                 Budget Request Form
               </a>
               <a
-                className="cursor-pointer"
+                className="cursor-pointer text-whitesmoke hover:text-black"
                 href="/pdfs/Reimbursement_Form.pdf"
                 download="Reimbursement_Form.pdf"
+                style={{ textDecoration: 'none' }}
               >
                 Reimbursement Form
               </a>
               <a
-                className="cursor-pointer"
+                className="cursor-pointer text-whitesmoke hover:text-black"
                 href="/pdfs/SA_Tech_Advance_Settlement_Form.pdf"
                 download="SA_Tech_Advance_Settlement_Form.pdf"
+                style={{ textDecoration: 'none' }}                
               >
                 Advance Settlement Form
               </a>
               <a
-                className="cursor-pointer"
+                className="cursor-pointer text-whitesmoke hover:text-black"
                 href="/pdfs/SA_Tech_03_ASSEST_TRANSFER_FORM.pdf"
                 download="SA_Tech_03_ASSEST_TRANSFER_FORM.pdf"
+                style={{ textDecoration: 'none' }}
               >
                 Asset Transform Form
               </a>
               <a
-                className="cursor-pointer"
+                className="cursor-pointer text-whitesmoke hover:text-black"
                 href="/pdfs/SA_Tech_03_ASSET_TRANSFER_FORM.pdf"
                 download="SA_Tech_Non_Consumable_Indent.pdf"
+                style={{ textDecoration: 'none' }}
               >
                 Non Consumable Indent Form
               </a>
               <a
-                className="cursor-pointer"
+                className="cursor-pointer text-whitesmoke hover:text-black"
                 href="/pdfs/SA_Tech_Temporary_Advance_Form.pdf"
                 download="SA_Tech_Temporary_Advance_Form.pdf"
+                style={{ textDecoration: 'none' }}
               >
                 Temporary Advance Form
               </a>
             </div>
           </div>
 
-          <div className="flex flex-col items-start gap-6 text-whitesmoke">
-            <div className="font-semibold">I2R MakerSpace</div>
+          <div className="flex flex-col items-start gap-6 text-whitesmoke mt-4">
+            <div className="font-semibold text-left"><h3> I2R MakerSpace</h3></div>
             <a
-              className="cursor-pointer"
+              className="cursor-pointer  text-whitesmoke hover:text-black"
               href="https://docs.google.com/document/d/1r8gTKgbhtiYPMCVVTHUTe97uFcEpFMG-e1pJwtALNEI/edit#heading=h.4hwppuotdne3"
               target="_blank"
               rel="noopener noreferrer"
+              style={{ textDecoration: 'none' }}
             >
               Access Form
             </a>
           </div>
         </div>
+
+
+
+
+
+
       </div>
       <hr className="border-t border-white w-full" />
-      <div className="text-center py-4 flex flex-col items-center justify-center lg:flex-row justify-between">
+      <div className="text-center py-4 flex flex-col items-center justify-between lg:flex-row ">
         <p className="text-xs sm:text-sm lg:text-base text-whitesmoke">
           © Technical Affairs IIITDM. All rights reserved.
         </p>
